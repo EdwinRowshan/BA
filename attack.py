809d6b62a4f16188b3b79db26b94f0dc7e115c77
Edwin <edwin.mahmoudrowshanzamirkou@campus.tu-berlin.de>
2024-05-29 21:56:58 +0200

    updated with connecting code

--- attack.py
+++ attack.py
@@ -1,5 +1,38 @@
 import chipwhisperer as cw
 import numpy as np
+import time
+
+PLATFORM = CW308_STM32F4
+
+try:
+    if not scope.connectStatus:
+        scope.con()
+except NameError:
+    scope = cw.scope()
+   
+try:
+    target = cw.target(scope)
+except IOError:
+    print("INFO: Caught exception on reconnecting to target - attempting to reconnect to scope first.")
+    print("INFO: This is a work-around when USB has died without Python knowing. Ignore errors above this line.")
+    scope = cw.scope()
+    target = cw.target(scope)
+
+print("INFO: Found ChipWhisperer😍")
+
+prog = cw.programmers.STM32FProgrammer
+
+time.sleep(0.05)
+scope.default_setup()
+
+def reset_target(scope): 
+    scope.io.nrst = 'low'
+    time.sleep(0.05)
+    scope.io.nrst = 'high_z'
+    time.sleep(0.05)
+
+#FLASH FIRMWARE HERE INSTEAD OF USING SELF WRITTEN FLASHER
+cw.program_target(scope, prog, "PATH OF FIRMWARE".format(PLATFORM))
 
 def cap_pass_trace(pass_guess):
     reset_target(scope)
